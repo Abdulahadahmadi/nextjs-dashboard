053ad97ce2719ce3f9dbf704185a001f077324f1
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-04 09:00:44 +0200

    add new files

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Edu_TAS_Beginner } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+})
+
+export const edu_tas_beginner = Edu_TAS_Beginner({
+    weight: ['400', '700'],
+    subsets: ['latin']
+})
